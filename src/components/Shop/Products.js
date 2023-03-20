b0b9708a67f402550ab13b230767dc7840f32500
bhoomi.raval <bhoomi.raval@seaflux.tech>
2023-03-20 16:10:06 +0530

    Initial Commit

---
+++ src/components/Shop/Products.js
@@ -0,0 +1,37 @@
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCT = [
+  {
+    id: "p1",
+    price: 6,
+    title: "my first book",
+    description: "first book I ever wrote",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "my second book",
+    description: "second book I ever wrote",
+  },
+];
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCT.map((product) => (
+          <ProductItem
+            key={product.key}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
